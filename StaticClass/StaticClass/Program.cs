ea331da6797acb0c6176ae80db023484a0abd092
Giovanni d'Amico <giovanni.damico@edu.unito.it>
2019-07-27 08:55:08 +0200

    Static class Using the Static System.[namespace]

---
+++ StaticClass/StaticClass/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StaticClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Static Utility Class *****");
+            TimeUtilsClass.PrintDate();
+            TimeUtilsClass.PrintTime();
+        }
+    }
+}
---
+++ StaticClass/StaticClass/TimeUtilsClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.DateTime;
+using static System.Console;
+
+
+namespace StaticClass
+{
+    static class TimeUtilsClass
+    {
+        public static void PrintTime()
+            => Console.WriteLine(Now.ToShortTimeString());
+        public static void PrintDate()
+            => Console.WriteLine(Today.ToShortDateString());
+    }
+}
