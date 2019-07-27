0346fefe68fce53e3a82ab528236da6862ec54a9
Giovanni d'Amico <giovanni.damico@edu.unito.it>
2019-07-27 18:22:52 +0200

    Exploring new features of C#: Chaining Builders, get set properties. The Encapsulation.

---
+++ EmployeeApp/EmployeeApp/Employee.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeApp
+{
+    
+class Employee
+    {
+        private string empName;
+        private int empID;
+        private float currPay;
+        private int empAge;
+        private string empSSN;
+        //BUILDER  -  chained builders with the ":" operator
+        public Employee() { }
+        public Employee(string name, int id, float pay) : this(name, 0, id, pay) { }
+
+        //USING THE PROPERTIES of C#, evitiamo di creare i metodi Get Set classic, e mi affido a quelli nuovi indicati
+        public Employee(string name, int age, int id, float pay, string ssn)
+        {
+            if (name > 15)
+                Console.WriteLine("Name too long, can set it");
+            else
+                Name = name;
+            Age = age;
+            ID = id;
+            Pay = pay;
+            empSSN = ssn;
+        }
+
+        public string Name
+        {
+            get => empName;
+            set
+            { if (value.Length > 15)
+                    Console.WriteLine("Error! Name Length exceeds 15 characters!");
+                else
+                    empName = value;
+            }
+        }
+
+        public int ID
+        {
+            get => empID;
+            set => empID = value;
+        }
+
+        public float Pay
+        {
+            get => currPay;
+            set => currPay = value;
+        }
+
+        public int Age
+        {
+            get => empAge;
+            set => empAge = value;
+        }
+
+        public string SocialSecurityNumber
+        {
+            get => empSSN;      //This is a ReadOnly parameter, so i avoid to describe a Set;
+        }
+        //METH
+        public void GiveBonus(float amount)
+        {
+            currPay += amount;
+        }
+
+        public void DisplayStats()
+        {
+            Console.WriteLine($"Name: {empName}; \nAge: {empAge} \nID: {empID}; \nPay: {currPay}");
+        }
+    }
+}
---
+++ EmployeeApp/EmployeeApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EmployeeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Encapsulation Samples ****");
+            Employee emp = new Employee("Marvin", 456, 30_000);
+            emp.GiveBonus(1000);
+            emp.DisplayStats();
+            /* Classic Method Get / Set
+            emp.setName("Marv");
+            Console.WriteLine("Emp name, has been changed now he is: {0}", emp.GetName());
+            */
+            emp.Name = "Marv"; //SET in C#
+            Console.WriteLine("Marvin name has changed in {0}", emp.Name);
+
+            //New get / set methods allows me to use easiest operation
+            Employee joe = new Employee("Joe", 457, 25_000);
+            joe.Age++;
+
+            joe.DisplayStats();
+        }
+    }
+}
