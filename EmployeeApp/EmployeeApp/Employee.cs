b57f5ea5df0144d51cab3c6f428c629655471460
Giovanni d'Amico <giovanni.damico@edu.unito.it>
2019-07-29 13:56:43 +0200

    Partial Class Contenente i Metodi (Core.cs) Partial Class Contenente i campi e il Builder (Employee.cs)

--- EmployeeApp/EmployeeApp/Employee.cs
+++ EmployeeApp/EmployeeApp/Employee.cs
@@ -5,7 +5,7 @@ using System.Text;
 namespace EmployeeApp
 {
     
-class Employee
+partial class Employee
     {
         private string empName;
         private int empID;
@@ -27,50 +27,6 @@ class Employee
             ID = id;
             Pay = pay;
             empSSN = ssn;
-        }
-
-        public string Name
-        {
-            get => empName;
-            set
-            { if (value.Length > 15)
-                    Console.WriteLine("Error! Name Length exceeds 15 characters!");
-                else
-                    empName = value;
-            }
-        }
-
-        public int ID
-        {
-            get => empID;
-            set => empID = value;
-        }
-
-        public float Pay
-        {
-            get => currPay;
-            set => currPay = value;
-        }
-
-        public int Age
-        {
-            get => empAge;
-            set => empAge = value;
-        }
-
-        public string SocialSecurityNumber
-        {
-            get => empSSN;      //This is a ReadOnly parameter, so i avoid to describe a Set;
-        }
-        //METH
-        public void GiveBonus(float amount)
-        {
-            currPay += amount;
-        }
-
-        public void DisplayStats()
-        {
-            Console.WriteLine($"Name: {empName}; \nAge: {empAge} \nID: {empID}; \nPay: {currPay}");
-        }
+        }      
     }
 }
